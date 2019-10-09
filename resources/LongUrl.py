5e8bf9a71dc3e40574132ee902654b4fc64398dd
Amit Kalay <amitkalay8@gmail.com>
2019-10-08 18:53:06 -0700

    simulated a cache response through a static variable

---
+++ resources/LongUrl.py
@@ -0,0 +1,23 @@
+from flask_restful import Resource
+from flask import *
+
+class LongUrl(Resource):
+    url_cache = dict()
+    #def __init__(self):
+    #    pass
+    def post(self):
+        data = request.get_json()
+        if "short_url" not in data:
+            return {'error': 'need a short_url key-value pair in the payload'}, 500
+
+
+        # try hitting the cache
+        short_url = data["short_url"]
+        if short_url in self.url_cache:
+            print("the url was in the cache!")
+            response = dict()
+            response["short_url"] = short_url
+            response["long_url"] = self.url_cache[short_url]
+            return jsonify(response)
+
+        return {'default': 'response'}
