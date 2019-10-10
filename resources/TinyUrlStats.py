3268923fb4ec3326134b8b85a718f5f03f5434e1
Amit Kalay <amitkalay8@gmail.com>
2019-10-10 01:07:40 -0700

    added remaining queries to tinyUrlStats endpoint and added an error handling scenario

--- resources/TinyUrlStats.py
+++ resources/TinyUrlStats.py
@@ -3,14 +3,24 @@ from flask_restful import Resource
 from model.ShortUrlAccess import ShortUrlAccess
 from datetime import datetime
 from datetime import timedelta
+
+
 # use timedelta to grab all the stats for a short url here !!
 
 
 class TinyUrlStats(Resource):
     def get(self):
         input_short_url = request.args.get('short_url')
+        if input_short_url is None:
+            return {'error': 'please add a short url query parameter like ?short_url=SOME_VALUE '}, 500
         hits_since_yesterday = self.get_last_24_hours_accesses(input_short_url)
-        return {'last_24_hour_accesses': hits_since_yesterday}
+        hits_since_last_week = self.get_past_week_accesses(input_short_url)
+        all_time_hits = self.get_all_time_accesses(input_short_url)
+        return {
+            'last_24_hour_accesses': hits_since_yesterday,
+            'last_week_accesses': hits_since_last_week,
+            'all_time_hits': all_time_hits
+        }
 
     def get_last_24_hours_accesses(self, input_short_url) -> int:
         now_date = datetime.now()
@@ -18,4 +28,16 @@ class TinyUrlStats(Resource):
         url_hits = ShortUrlAccess.objects(short_url=input_short_url,
                                           date_accessed__gte=yesterday)
 
+        return len(url_hits)
+
+    def get_past_week_accesses(self, input_short_url) -> int:
+        now_date = datetime.now()
+        last_week = now_date - timedelta(weeks=1)
+        url_hits = ShortUrlAccess.objects(short_url=input_short_url,
+                                          date_accessed__gte=last_week)
+
+        return len(url_hits)
+
+    def get_all_time_accesses(self, input_short_url) -> int:
+        url_hits = ShortUrlAccess.objects(short_url=input_short_url)
         return len(url_hits)
